46315bc4265f9efc778f7e2be2188cd8199dd4a4
Maajith <maajithanas@gmail.com>
2025-11-09 21:34:10 +0530

    Hi

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
 
+
+import { registerUser, loginUser } from "./controllers/authController.js";
+
 // ✅ Import Controllers
 import {
   startAttendance,
@@ -18,16 +21,24 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// ✅ Middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// ✅ MongoDB Connection
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log(`✅ MongoDB Connected: ${mongoose.connection.host}`))
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+
+
+
+// ✅ Auth Routes
+app.post("/api/register", registerUser);
+app.post("/api/login", loginUser);
+
+
 
 // ✅ Attendance Routes
 app.post("/api/attendance/start", startAttendance);
